fdc8375a653a4e4e0899ad63f7d2784c5345e8b2
jcv-dev <valenciarivasjuancamilo@gmail.com>
2025-05-13 02:19:17 -0500

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
     id("androidx.navigation.safeargs.kotlin")
-
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -56,9 +56,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.biometric.ktx)
-    val lottieVersion = "3.4.0"
-    implementation("com.airbnb.android:lottie:$lottieVersion")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +63,46 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Biometric
+    implementation("androidx.biometric:biometric-ktx:1.2.0-alpha04")
+
+    //NavComponent
+    val navVersion = "2.7.0"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //Fragmentos
+    val fragment_version = "1.7.0"
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
+    //User interface components
+    val materialVersion= "1.4.0"
+    implementation("com.google.android.material:material:$materialVersion")
+
+    //retrofit library
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    //room
+    val room_version = "2.5.2"
+    implementation ("androidx.room:room-ktx:$room_version")
+    implementation ("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    //implementation("androidx.lifecycle:lifecycle-runtime-ktx:$coroutinesVersion")
+
+    // LiveData
+    val liveDataVersion = "2.3.1"
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$liveDataVersion")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+
+    //lottiejson
+    val lottieVersion = "3.4.0"
+    implementation("com.airbnb.android:lottie:$lottieVersion")
 }
